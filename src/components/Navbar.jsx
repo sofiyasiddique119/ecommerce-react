a7171e6f2b05895dbda25bf09ccd2b9093e31780
Sofiya Siddique <sofiyasiddique119@gmail.com>
2025-02-04 15:57:41 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import "./components.css"
 import { SiBigcommerce } from "react-icons/si";
 import { FaCartShopping } from "react-icons/fa6";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 function Navbar() {
+  const navigate = useNavigate()
   return (
     <nav className="navbar"> <h1><SiBigcommerce />Basket</h1>
         <ul className="navbarlist">
@@ -12,7 +13,7 @@ function Navbar() {
 <li className="navbar-item"><Link to= "/about">About</Link></li>
 <li className="navbar-item"><a href="#">Contact</a></li>
         </ul>
-        <div className="navbar-act"><button className="login-btn">Log-in</button>
+        <div className="navbar-act"><button className="login-btn" onClick={()=>{navigate("/auth")}}>Log-in</button>
         <Link to="/cart"><h2 style={{color:"#a7a0a0"}}><FaCartShopping /></h2></Link>
         </div>
     </nav>
